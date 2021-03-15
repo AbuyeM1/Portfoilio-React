3ec500de9fd2cc54e37db8d070d6bab9e08c8167
Abuye Mamuye <abuyeye3@gmail.com>
2021-03-15 16:21:26 -0700

    creating a file navbar and footer

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./components/About";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
-import Portfolio from "./components/portfolio";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Route path="/contact" component={Contact} />
         <Footer />
       </div>
     </Router>
---
+++ portfolio-react/src/components/Navbar.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="container-nav">
+      <ul className="nav nav-tab">
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={
+              window.location.pathname === "/" ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/about"
+            className={
+              window.location.pathname === "/about"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            About
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/portfolio"
+            className={
+              window.location.pathname === "/portfolio"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Portfolio
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/contact"
+            className={
+              window.location.pathname === "/contact"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Contact
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/resume"
+            className={
+              window.location.pathname === "/resume"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Resume
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
