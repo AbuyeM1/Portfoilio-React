c16b015193bc447fb7cc75b305582fb5bc68fda6
Abuye Mamuye <abuyeye3@gmail.com>
2021-09-07 11:08:46 -0700

    Adding the file

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -6,7 +6,6 @@ import Footer from "./components/Footer";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
-
 import Contact from "./components/pages/Contact";
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
-
         <Route path="/contact" component={Contact} />
         <Footer />
       </div>
